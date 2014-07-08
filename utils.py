68feab752718298a8bfba489f2730151d4b62b33
Ivan Alejandro <ivanalejandro0@gmail.com>
2014-07-08 17:02:41 -0300

    Use multiline docstrings.

--- utils.py
+++ utils.py
@@ -12,8 +12,9 @@ KEYS_DIR = 'zmq_certificates'
 
 
 def generate_certificates():
-    """ Generate client and server CURVE certificate files """
-
+    """
+    Generate client and server CURVE certificate files.
+    """
     # Create directory for certificates, remove old content if necessary
     if os.path.exists(KEYS_DIR):
         shutil.rmtree(KEYS_DIR)
@@ -26,14 +27,18 @@ def generate_certificates():
 
 
 def get_frontend_certificates():
-    """ Return the frontend's public and secret certificates. """
+    """
+    Return the frontend's public and secret certificates.
+    """
     frontend_secret_file = os.path.join(KEYS_DIR, "frontend.key_secret")
     public, secret = zmq.auth.load_certificate(frontend_secret_file)
     return public, secret
 
 
 def get_backend_certificates(base_dir='.'):
-    """ Return the backend's public and secret certificates. """
+    """
+    Return the backend's public and secret certificates.
+    """
     backend_secret_file = os.path.join(KEYS_DIR, "backend.key_secret")
     public, secret = zmq.auth.load_certificate(backend_secret_file)
     return public, secret
